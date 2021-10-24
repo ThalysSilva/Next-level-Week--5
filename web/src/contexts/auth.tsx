03b887fd7cd1b0b567bf19f66ba0eb10e0b3a5a2
ThalysSilva <65621610+ThalysSilva@users.noreply.github.com>
2021-10-23 22:53:58 -0300

    LoginBox, AuthContext
    finalização do login box, e a  inserção do AuthContext na aplicação.

---
+++ web/src/contexts/auth.tsx
@@ -0,0 +1,103 @@
+import { createContext, ReactNode, useState } from "react";
+import { useEffect } from "react"
+import { api } from "../services/api"
+
+type AuthResponse = {
+
+    token: string;
+    user: {
+        id: string;
+        avatar_url: string;
+        name: string;
+        login: string;
+    }
+
+}
+
+type User = {
+
+    id: string;
+    name: string;
+    login: string;
+    avatar_url: string;
+}
+
+type AuthContextData = {
+    user: User | null;
+    signInUrl: string;
+    signOut: () => void;
+}
+
+
+
+export const AuthContext = createContext({} as AuthContextData)
+
+type AuthProvider = {
+    children: ReactNode;
+}
+
+
+export function AuthProvider(props: AuthProvider){
+
+    const [user, setUser] = useState<User | null>(null);
+
+    const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=47409ec153d853add099`
+
+    async function signIn(githubCode: string) {
+
+        const response = await api.post<AuthResponse>('authenticate', {
+            code: githubCode,
+        })
+
+        const{ token, user } = response.data;
+
+        localStorage.setItem("@doWhile:token", token)
+
+        setUser(user)
+
+    }
+
+    function signOut(){
+        setUser(null)
+        localStorage.removeItem("@doWhile:token")
+    }
+
+    useEffect(() => {
+        const token = localStorage.getItem("@doWhile:token")
+
+        if(token){
+            api.defaults.headers.common.authorization = `Bearer ${token}`;
+
+            api.get<User>('profile').then(response => {
+                setUser(response.data)
+            })
+
+        }
+
+    }, [])
+    
+    
+    useEffect(() => {
+        const url = window.location.href;
+        const hasGitHubCode = url.includes("?code=");
+
+        if(hasGitHubCode) {
+            const [urlWithoutCode, githubCode] = url.split("?code=")
+
+            window.history.pushState({},'',urlWithoutCode);
+
+            signIn(githubCode)
+
+        }
+    },[])
+
+    return(
+
+        <AuthContext.Provider value={{signInUrl, user, signOut}}>
+
+            {props.children}
+
+        </AuthContext.Provider>
+    )
+
+}
